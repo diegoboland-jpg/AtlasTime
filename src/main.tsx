e639a9718341b56379ee5ea4e45c97368794c121
diegoboland-jpg <diego.boland@gmail.com>
2026-08-13 18:38:02 -0300

    fix: wire Kikiroo branding into app startup

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { registerAtlasTimeServiceWorker } from "./pwa";
+import { applyKikirooBrand } from "./kikirooBrand";
 import "./styles.css";
+import "./kikiroo-brand.css";
 
+applyKikirooBrand();
 registerAtlasTimeServiceWorker();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
